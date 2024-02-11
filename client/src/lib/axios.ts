713a59e6fca8e9ce2b07dc943564e82c866d1402
Noldor99 <qwerr19942017@gmail.com>
2024-02-11 16:14:00 +0200

    fix

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const sokerUrl = process.env.NEXT_PUBLIC_API_URL_SOKET ?? 'https://chat-back-yvv3.onrender.com/api/'
+export const sokerUrl = process.env.NEXT_PUBLIC_API_URL_SOKET ?? 'https://chat-back-yvv3.onrender.com'
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'https://chat-back-yvv3.onrender.com/api/',
