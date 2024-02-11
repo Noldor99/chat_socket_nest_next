9471ab46051ed5f1cae77219c6d29ca998d74cd7
Noldor99 <qwerr19942017@gmail.com>
2024-02-11 12:33:26 +0200

    domains

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "localhost",
+      "https://chat-back-yvv3.onrender.com",
+      "loremflickr.com",
+    ],
+  },
+}
 
 module.exports = nextConfig
