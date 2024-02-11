35e086f7122a6e4bbd1e2fa6a4d70cc3e5adbca6
Noldor99 <qwerr19942017@gmail.com>
2024-02-11 13:08:09 +0200

    simpled

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'https://chat-back-yvv3.onrender.com/api',
+
   withCredentials: true,
 })
